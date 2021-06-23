5c228b45c019d9edda6ae9f3a2fdd47d5b7a742e
Cameron Houck <cameronhouck1@gmail.com>
2021-06-23 18:21:39 -0400

    adding package variables, connecting to database and doing npm install

---
+++ server.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql");
+const inquirer = require ("inquirer");
+const util = require ("util");
+const conTable = require("console.table");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    port: 3306,
+
+    user: 'root',
+
+    password: '200284',
+
+    database: 'employee_trackerDB',
+});
+console.log("you are connected")
