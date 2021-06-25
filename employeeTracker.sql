8bd3f6823584820805a369b21d7bfc350ef3b08f
Cameron Houck <cameronhouck1@gmail.com>
2021-06-24 22:30:27 -0400

    adding database tables

---
+++ employeeTracker.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS trackEmployeeDB;
+CREATE database employee_trackerDB;
+
+USE trackEmployeeDB;
+
+CREATE TABLE department (
+  ID INT NOT NULL AUTO_INCREMENT,
+  dept_name VARCHAR(30) NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE emp_role (
+  ID INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,2) NULL,
+  dept_id INT NULL REFERENCES department(ID),
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE employee (
+  ID INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL REFERENCES emp_role(ID),
+  manager_id INT NULL REFERENCES employee(ID),
+  PRIMARY KEY (ID)
+);
+
+SELECT * FROM department;
+SELECT * FROM emp_role;
+SELECT * FROM employee;
